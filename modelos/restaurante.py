5be316c3993e8843fb1242a2e114c92624c6aeb5
Dayana Souza <dayanalvesouza@gmail.com>
2024-03-22 12:23:12 -0300

    criado o metodo str para exibir informações

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -6,10 +6,13 @@ class Restaurante:
         self.categoria = categoria
         self.ativo = False
 
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
 #Objetos
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(vars(restaurante_praca))
+print(restaurante_praca)
+print(restaurante_pizza)
