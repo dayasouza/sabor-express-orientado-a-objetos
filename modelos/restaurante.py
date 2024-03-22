258308547d4c5f59253b67ca25403d313e400dfa
Dayana Souza <dayanalvesouza@gmail.com>
2024-03-22 15:38:26 -0300

    excluido um arquivo

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,27 +1,33 @@
 class Restaurante:
     #lista de restaurantes
     restaurantes = []
+
     #construtor
     def __init__(self, nome, categoria):
         #Atributos 
-        self.nome = nome
-        self.categoria = categoria
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
         self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
 
-    def listar_restaurantes():
+    @classmethod
+    def listar_restaurantes(cls):
         print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante._ativo}')
 
     @property
     def ativo(self):
         return 'ativo' if self._ativo else 'inativo'
+
+    def alternar_estado(self):
+        self._ativo = not self._ativo 
 #Objetos
 restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_praca.alternar_estado()
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
 Restaurante.listar_restaurantes()
