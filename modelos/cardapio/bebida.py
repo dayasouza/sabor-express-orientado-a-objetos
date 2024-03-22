e4a0cada4f317e7bb35a2d7b6a492b74f9e2668d
Daya Souza <dayanalvesouza@gmail.com>
2024-03-22 19:05:55 -0300

    criado a classe filha bebida do cardapio

---
+++ modelos/cardapio/bebida.py
@@ -0,0 +1,8 @@
+from modelos.cardapio.item_cardapio import ItemCardapio
+class Bebiba(ItemCardapio):
+    def __init__(self, nome, preco, tamanho):
+        super().__init__(nome, preco)
+        self.tamanho = tamanho
+        
+    def __str__(self):
+        return self._nome
