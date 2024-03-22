b1286ee276f959fc3059e61ed4f69c5cbcf38bf9
Dayana Souza <dayanalvesouza@gmail.com>
2024-03-22 14:33:37 -0300

    usado @property para modificar o atributo ativo

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -6,15 +6,20 @@ class Restaurante:
         #Atributos 
         self.nome = nome
         self.categoria = categoria
-        self.ativo = False
+        self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
 
     def listar_restaurantes():
+        print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+
+    @property
+    def ativo(self):
+        return 'ativo' if self._ativo else 'inativo'
 #Objetos
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
