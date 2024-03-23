04865bab81a1cf8471c610df850ed82f247bc252
Daya Souza <dayanalvesouza@gmail.com>
2024-03-23 14:08:46 -0300

    crido o metodo de desconto utilizando polimorfismo

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ from modelos.restaurante import Restaurante
 from modelos.cardapio.bebida import Bebiba
 from modelos.cardapio.prato import Prato
 
+
 #Criando e instanciando os restaurantes
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 bebida_suco = Bebiba('Suco de Melancia', 5.0, 'grande')
+bebida_suco.aplicar_desconto()
 prato_paozinho = Prato('Pãozinho', 2.00, 'O melhor pão da cidade')
+bebida_suco.aplicar_desconto()
 restaurante_praca.adicionar_no_cardapio(bebida_suco)
 restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
